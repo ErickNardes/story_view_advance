d1a8e300419a1a7b1b033129d563208d218cbed9
Erick <erick.lima@posthaus.com.br>
2025-02-04 09:50:10 -0300

    remo full height

--- lib/widgets/story_video.dart
+++ lib/widgets/story_video.dart
@@ -171,8 +171,6 @@ class StoryVideoState extends State<StoryVideo> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.black,
-      height: double.infinity,
-      width: double.infinity,
       child: getContentView(),
     );
   }
