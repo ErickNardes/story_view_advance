e68d6b28e4da026148f4a0feea3ea8f317a796c8
Erick <erick.lima@posthaus.com.br>
2025-02-04 10:44:01 -0300

    add fittedbox and removecenter

--- lib/widgets/story_video.dart
+++ lib/widgets/story_video.dart
@@ -137,14 +137,12 @@ class StoryVideoState extends State<StoryVideo> {
   Widget getContentView() {
     if (widget.videoLoader.state == LoadState.success &&
         playerController!.value.isInitialized) {
-      return Center(
-        child: FittedBox(
-          fit: BoxFit.contain,
-          child: SizedBox(
-            width: playerController!.value.size.width,
-            height: playerController!.value.size.height,
-            child: VideoPlayer(playerController!),
-          ),
+      return FittedBox(
+        fit: BoxFit.contain,
+        child: SizedBox(
+          width: playerController!.value.size.width,
+          height: playerController!.value.size.height,
+          child: VideoPlayer(playerController!),
         ),
       );
     }
